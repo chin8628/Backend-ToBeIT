56bc41fb58073a7f58d48479fafe756be54d80c2
chin8628 <boon.8628@gmail.com>
2016-01-13 11:36:19 +0700

    Add food and Sheet dropdown

--- application/views/come.php
+++ application/views/come.php
@@ -3,46 +3,75 @@
         <div class="page-header">
             <h1>Check In</h1>
         </div>
-        <section class="row">
-            <div class="col-sm-8">
-                <div class="panel panel-default">
-                    <div class="panel-heading">ข้อมูลน้องโดยสังเขป</div>
-                    <div class="panel-body">
-                        <p>รหัสประจำตัว: {id_student}</p>
-                        <p>ชื่อ - นามสกุล: {prename} {name} {surname}</p>
-                        <p>ชื่อเล่น: {nickname}</p>
-                        <p>โรงเรียน: {school}</p>
-                        <p>ศาสนา: {religion}</p>
-                        <p>ไซส์เสื้อ: {shirt}</p><br>
-                        <table class="table table-bordered text-center">
-                            <thead>
-                                <tr>
-                                    {chk_thead}
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    {chk_tbody}
-                                </tr>
-                            </tbody>
-                        </table>
+        <form method="post">
+            <section class="row">
+                <div class="col-sm-8">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">ข้อมูลน้องโดยสังเขป</div>
+                        <div class="panel-body">
+                            <p>รหัสประจำตัว: {id_student}</p>
+                            <p>ชื่อ - นามสกุล: {prename} {name} {surname}</p>
+                            <p>ชื่อเล่น: {nickname}</p>
+                            <p>โรงเรียน: {school}</p>
+                            <p>ศาสนา: {religion}</p>
+                            <p>ไซส์เสื้อ: {shirt}</p><br>
+                            <table class="table table-bordered text-center">
+                                <thead>
+                                    <tr>
+                                        {chk_thead}
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        {chk_tbody}
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-sm-4">
-                <div class="panel panel-default">
-                    <div class="panel-heading">เลือกห้องเรียน</div>
-                    <div class="panel-body">
-                        <form method="post">
+
+                <div class="col-sm-4">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">เลือกเมนูอาหาร</div>
+                        <div class="panel-body">
+                            <div class="form-group">
+                                <label for="room">เมนูอาหาร </label>
+                                {menu_food}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">เลือกห้องเรียน</div>
+                        <div class="panel-body">
                             <div class="form-group">
                                 <label for="room">ห้องเรียน </label>
                                 {classroom}
                             </div>
-                            <button type="submit" class="btn btn-success">Checkin</button>
-                        </form>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+            <section class="row">
+                <div class="col-sm-8">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">เลือกเอกสารการเรียน</div>
+                        <div class="panel-body">
+                            {sheet_dropdown}
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-4">
+                    <div class="panel panel-default">
+                        <div class="panel-body">
+                            <button type="submit" class="btn btn-success btn-lg">Checkin</button>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </form>
     </div>
 </body>
