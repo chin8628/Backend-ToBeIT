f8466cb7f66f4718b282708fe8108eb9d7f2835b
chin8628 <boon.8628@gmail.com>
2016-01-16 01:10:45 +0700

    reverse config to localhost

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://tobe.it.kmitl.ac.th/2015/backend/';
+$config['base_url'] = 'http://localhost/backend/';
 
 /*
 |--------------------------------------------------------------------------
